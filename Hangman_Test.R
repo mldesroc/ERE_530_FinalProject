7876446f392ba413b0591e9462b69638b51deb8e
Madeleine Desrochers <mdesrochers11@gmail.com>
2021-12-02 15:12:44 -0500

    gameplay improvements

--- Hangman_Test.R
+++ Hangman_Test.R
@@ -1,7 +1,6 @@
 suppressPackageStartupMessages(library(proceduralnames))
 suppressPackageStartupMessages(library(here))
 suppressPackageStartupMessages(library(magick))
-suppressMessages(message("ABC"))
 
 word <- make_english_names(1,1)
 word_split<- unlist(strsplit(word,""))
@@ -61,19 +60,21 @@ for (i in 1:length(word)){
   return(b)
 }
 
-
 incorrect_guesses <- 0
 correct_guesses <- 0
 correctly_guessed_letters <- character(0)
 plot(snowman1)
+
 print("Welcome to the Snowman Game! Its up to you to correctly guess the secret word and save the snowman from a watery end!")
 print("Lets begin!")
+print(character(length(word_split)))
 while ((correct_guesses < length(unique(word_split))) & (incorrect_guesses < 9)) {
   guess <- readline("Enter your guess: ")
   match <- checker(word_split, guess)
       if (match==FALSE){
         print("incorrect") 
         incorrect_guesses <- incorrect_guesses + 1
+        print(word_filler(correctly_guessed_letters, word_split))
         snowman_selector(incorrect_guesses)
     } else {
         print("correct")
@@ -84,9 +85,10 @@ while ((correct_guesses < length(unique(word_split))) & (incorrect_guesses < 9))
  }
  if (correct_guesses == length(unique(word_split))){
    print("Congradulations! You've saved the snowman!")
+   plot(snowman1)
  } else if (incorrect_guesses == 9) {
-   print ("Oh No! You've melted the snowman!")
+   print (paste("Oh No! You've melted the snowman! The secret word was", word))
  }
  
-again <- readline("Would you like to play again?")
-if(again == "y"){source("Hangman_Test.R")}
+again <- readline("Would you like to play again? Enter yes or no ")
+if(again == "yes"){source("Hangman_Test.R")}
